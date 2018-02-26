37b1ade7b9484621b96a7aef5eb71711ef2a6ad8
GerardoA <luceroriosg@gmail.com>
2018-02-26 09:03:58 -0700

    Modificando app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,17 +9,17 @@ console.log('Custom scripts');
 // });
 
 
-import Echo from "laravel-echo";
-import io from "socket.io-client";
-window.io = io;
+// import Echo from "laravel-echo";
+// import io from "socket.io-client";
+// window.io = io;
 
-window.Echo = new Echo({
-	broadcaster: 'socket.io',
-	host: 'http://blog.dev:6001'
-});
+// window.Echo = new Echo({
+// 	broadcaster: 'socket.io',
+// 	host: 'http://blog.dev:6001'
+// });
 
 
-window.Echo.channel('messages.channel');
-window.Echo.listen('MessageWasReceived', (data) =>{
-		console.log(data);
-});
+// window.Echo.channel('messages.channel');
+// window.Echo.listen('MessageWasReceived', (data) =>{
+// 		console.log(data);
+// });
